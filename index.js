150f8569faf3bdb140a8ea3f716d279031aef8d1
Dharmendra Yadav <yaduvanshi.dhanno@gmail.com>
2024-09-03 23:14:41 +0530

    New function isAdult added

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const name = "Dharmendra Yadav";
+const name_my = "Dharmendra Yadav";
 const age = 21;
 
 const dob= "29-09-2003";
 const hobby = "rapping";
 
 function getName(){
-    console.log(`My name is: ${name}`);
+    console.log(`My name is: ${name_my}`);
+}
+
+function isAdult(){
+    return age>=18;
 }
